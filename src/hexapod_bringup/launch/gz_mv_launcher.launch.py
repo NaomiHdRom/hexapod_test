aee5157be377aa5d445356cc484b9a4a330eea1b
Naomi HdRom <naomi.hernandez@ingenieria.unam.edu>
2026-02-16 19:05:36 -0600

    Ya abre Gazabo+Rviz con el motion planing abierto

--- src/hexapod_bringup/launch/gz_mv_launcher.launch.py
+++ src/hexapod_bringup/launch/gz_mv_launcher.launch.py
@@ -11,11 +11,16 @@ import xacro
 def generate_launch_description():
 
     # ----------------------------
-    # Paths
+    # Packages
     # ----------------------------
     bringup_pkg = get_package_share_directory('hexapod_bringup')
     moveit_pkg = get_package_share_directory('hexapod_moveit_config_new')
-
+    rviz_config = os.path.join(moveit_pkg, 'config', 'moveit.rviz')
+   
+  
+    # ----------------------------
+    # Paths
+    # ----------------------------
     xacro_file = os.path.join(moveit_pkg, 'config', 'hexapod.urdf.xacro')
     world_file = os.path.join(bringup_pkg, 'world', 'hexapod_world.world')
 
@@ -102,13 +107,24 @@ def generate_launch_description():
     )
 
     # ----------------------------
+    # TF estático world -> base_footprint para el virtual_joint de MoveIt
+    # ----------------------------
+    world_tf = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=['0', '0', '0', '0', '0', '0', 'world', 'base_footprint'],
+        output='screen'
+    )
+
+    # ----------------------------
     # RViz (solo uno, no doble)
     # ----------------------------
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
         output='screen',
-        parameters=[{'use_sim_time': True}]
+        arguments=['-d', rviz_config],
+        parameters=[{'use_sim_time': True}],
     )
 
     # ----------------------------
@@ -120,5 +136,6 @@ def generate_launch_description():
         spawn_entity_handler,
         *controller_spawners,
         moveit_launch,
+        world_tf,
         rviz_node,
     ])
